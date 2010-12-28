426dd71ed1bc3d6f83fe9266b58086300a01e94e
Ryan Cook <ryan@quickleft.com>
2010-12-28 15:28:59 -0700

    fixup the routing setup and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
   # This adds our engine's routes to the host rails application scoped to our engine module
-  scope :module => :aperio do
+  scope Aperio.route_namespace , :module => :aperio , :as => :aperio do
 
     # Add our HTTP GET only index route that a client will direct a user to
-    match "#{Aperio.route_namespace}", :to => 'oauth#index', :as => :aperio_oauth_index, :via => :get
+    match "/", :to => 'oauth#index', :as => :oauth_index, :via => :get
 
     # Add our HTTP POST only authenticate route that the authentication form will post to
-    match "#{Aperio.route_namespace}/authenticate", :to => 'oauth#authenticate', :as => :aperio_oauth_authenticate, :via => :post
+    match "authenticate", :to => 'oauth#authenticate', :as => :oauth_authenticate, :via => :post
 
   end
 
--- test/routes_test.rb
+++ test/routes_test.rb
@@ -2,14 +2,22 @@ require 'test_helper'
 
 class DefaultRoutingTest < ActionController::TestCase
 
+  # Setup our tests
+  def setup
+    Aperio.setup do |config|
+      config.route_namespace = "a_test_namespace"
+    end
+    Dummy::Application.reload_routes!
+  end
+
   # This tests the route that the user is sent to authorize a client's access to protected resources
   test 'map oauth index' do
-     assert_routing( { :path => 'oauth' , :method => :get } , { :controller => 'aperio/oauth' , :action => 'index' } )
+     assert_routing( { :path => Aperio.route_namespace , :method => :get } , { :controller => 'aperio/oauth' , :action => 'index' } )
   end
 
   # This tests the route that user credentials will be HTTP POSTed to to authenticate on the resource server
   test 'map oauth authenticate' do
-    assert_routing( { :path => 'oauth/authenticate' , :method => :post } , { :controller => 'aperio/oauth' , :action => 'authenticate' } )
+    assert_routing( { :path => "#{Aperio.route_namespace}/authenticate" , :method => :post } , { :controller => 'aperio/oauth' , :action => 'authenticate' } )
   end
 
 end
